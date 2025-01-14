a23473fc0a0c4760dd4436711debc358fc4a7974
ciperuoy <long220352@icloud.com>
2025-01-13 19:42:25 -0500

    v1.0.0

---
+++ 2toDisconnect/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _2toDisconnect
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        public void Update()
+        {
+            bool Button1 = ControllerInputPoller.instance.leftControllerSecondaryButton || Keyboard.current.yKey.isPressed;
+            bool Button2 = ControllerInputPoller.instance.rightControllerSecondaryButton || Keyboard.current.bKey.isPressed;
+
+            if (Button1 && Button2) // Y and B
+            {
+                NetworkSystem.Instance.ReturnToSinglePlayer(); // I could try PhotonNetwork.Disconnect();
+                Debug.Log("Successfully disconnected.");
+            }
+            else if (Button1 && Button2 && PhotonNetwork.InRoom) // If the buttons are pressed, but still in room, send error.
+            {
+                Debug.LogError("Disconnect failed.");
+            }
+            else if (!PhotonNetwork.InRoom)
+            {
+                Debug.LogError("Error: Not in lobby!");
+            }
+        }
+    }
+}
---
+++ 2toDisconnect/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace _2toDisconnect
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.ciperuoy.gorillatag.2todisconnect";
+        public const string Name = "2toDisconnect";
+        public const string Version = "1.0.0";
+    }
+}
